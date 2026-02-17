b6dc551c97e1af63c845a5376834f14f2d090db1
gadeergatasha <gatashagadeer@gmail.com>
2026-02-17 21:18:03 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,89 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+import time
+
+# =============================
+# Load Model
+# =============================
+model = tf.keras.models.load_model("pneumonia_model_final.keras")
+
+# =============================
+# Page Config
+# =============================
+st.set_page_config(
+    page_title="AI Pneumonia Detection",
+    page_icon="🩺",
+    layout="centered"
+)
+
+st.title("🩺 AI-Based Pneumonia Detection System")
+st.markdown("### Chest X-ray Classification using Deep Learning")
+
+st.write(
+    "Upload a chest X-ray image and the AI model will classify it."
+)
+
+st.info(
+    "⚠️ This system is designed to assist medical professionals in diagnosis. "
+    "It is NOT intended to replace clinical judgment or professional medical decisions."
+)
+
+st.divider()
+
+uploaded_file = st.file_uploader(
+    "📤 Upload Chest X-ray Image",
+    type=["jpg", "png", "jpeg"]
+)
+
+if uploaded_file is not None:
+
+    image = Image.open(uploaded_file).convert("RGB")
+    st.image(image, width="stretch")
+
+    img = image.resize((224, 224))
+    img_array = np.array(img) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+
+    st.divider()
+
+    if st.button("🔍 Analyze Image"):
+
+        with st.spinner("Analyzing image using AI model..."):
+            progress = st.progress(0)
+
+            for i in range(100):
+                time.sleep(0.01)
+                progress.progress(i + 1)
+
+            prediction = model.predict(img_array)
+            confidence = float(prediction[0][0])
+
+        st.subheader("📊 Prediction Result")
+
+        # =============================
+        # 3-Level Decision System
+        # =============================
+
+        if confidence >= 0.70:
+            st.error("🦠 Pneumonia Detected")
+            st.write(f"Confidence: **{confidence * 100:.2f}%**")
+            st.progress(int(confidence * 100))
+
+        elif confidence <= 0.40:
+            st.success("✅ Normal")
+            normal_conf = 1 - confidence
+            st.write(f"Confidence: **{normal_conf * 100:.2f}%**")
+            st.progress(int(normal_conf * 100))
+
+        else:
+            st.warning("⚠️ Uncertain – Requires Medical Review")
+            st.write(f"Model Output: **{confidence * 100:.2f}%**")
+            st.progress(int(confidence * 100))
+
+    if st.button("🔄 Reset"):
+        st.rerun()
+
+st.markdown("---")
+st.caption("Graduation Project | Deep Learning for Medical Image Classification")
